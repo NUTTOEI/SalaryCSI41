2bc2c56d7bfb13ca583f977dcd558eb2d5402a15
NUTTOEI <nut0863481103@gmail.com>
2026-08-18 09:31:40 +0700

    UserID เพลง

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN;
 const LINE_TARGET_IDS = [
     process.env.LINE_TARGET_IDS,
     'Ufac721db10fe012f12410f3cf59c3eb7',
-    'Uf963df571b9d63db04690e4801fe1439'
+    'Uf963df571b9d63db04690e4801fe1439',
+    'U240ca012882ab72b27e01377a6554632'
 ]
 const EXPECTED_RECEIVER_NAME = "ณัฐวัฒน์ สุดพูล";
 
