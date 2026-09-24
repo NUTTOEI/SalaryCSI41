dece5f26d5f58878ba11b9e5149cddb75e3f97fa
NUTTOEI <nut0863481103@gmail.com>
2026-09-24 22:54:22 +0700

    19

--- app.js
+++ app.js
@@ -697,7 +697,7 @@ app.use((err, req, res, next) => {
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'member.html'));
 });
 
 const PORT = process.env.PORT || 3000;
